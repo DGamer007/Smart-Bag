fe8a7f15b74ed7d60cf14fe7c37b97bb865cc14d
DGamer007 <prajapatidhruv266@gmail.com>
2021-07-07 09:24:25 +0530

    changes in server routes

--- server/server.js
+++ server/server.js
@@ -9,13 +9,13 @@ const publicDir = path.join(__dirname, '../public')
 app.use(express.static(publicDir))
 app.use(express.json())
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
-
 app.get('/userdata/:id', async (req, res) => {
     const data = fetchNecessaryData(req.params.id)
     res.send(data)
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.listen(port, () => {
