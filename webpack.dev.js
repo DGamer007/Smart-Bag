27b69f11d3b56e078c2d5901dff24a2ffdc9d820
DGamer007 <prajapatidhruv266@gmail.com>
2021-08-26 12:22:37 +0530

    configs for webpack-dev-server to work on local network and also responsive css

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,10 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
+const os = require('os')
+
+const port = 8080
+const onNetwork = os.networkInterfaces()['Wi-Fi'][1].address
 
 module.exports = merge(common, {
     mode: 'development',
@@ -12,8 +16,18 @@ module.exports = merge(common, {
     ],
     devtool: 'inline-source-map',
     devServer: {
+        noInfo: true,
+        onListening() {
+            console.log(`Local Connection: http://localhost:${port}`)
+            console.log(`On your Network: http://${onNetwork}:${port}`)
+        },
         contentBase: path.join(__dirname, 'public'),
         publicPath: '/dist/',
+        host: '0.0.0.0',
+        port,
+        open: true,
+        openPage: `http://localhost:${port}`,
+        hot: true,
         historyApiFallback: true
     }
 })
